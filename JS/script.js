c8a20530ce3a932bf8ee63259907f16ccdf27cde
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-05-16 14:37:27 +0545

    Completed index.html, js and css

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,18 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const ctx = document.getElementById("chart").getContext("2d");
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
-      datasets: [
-        {
-          label: "Label 1",
-          data: [10, 12, 8, 14, 10, 18],
-          borderColor: "#6366f1",
-          tension: 0.4,
+      const ctx = document.getElementById("analyticsChart").getContext("2d");
+      const analyticsChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+          datasets: [
+            {
+              label: "Income",
+              data: [5000, 7000, 4000, 8000, 9000, 10000, 11000],
+              borderColor: "#4b7bec",
+              fill: false,
+              tension: 0.4,
+            },
+          ],
         },
-        {
-          label: "Label 2",
-          data: [5, 6, 7, 5, 6, 7],
-          borderColor: "#facc15",
-          tension: 0.4,
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-    },
-  });
-});
+      });
+    
